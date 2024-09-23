2fbe3df5949695dbf524759a7ff8735b7aa531df
hennach <mohammed.hennach@univ-rouen.fr>
2024-09-23 14:32:39 +0200

    en cours

--- TP4/data.sql
+++ TP4/data.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE demosql;
 USE demosql;
-CREAT TABLE myTable(id int AUTO_INCREMENT, name varchar(45) NOT NULL
-	, PRIMARY KEY (id) );
+CREATE TABLE myTable(id int AUTO_INCREMENT, name varchar(45) NOT NULL, PRIMARY KEY (id) );
 INSERT INTO myTable (id, name) VALUES (null, 'bob');
 INSERT INTO myTable (id, name) VALUES (null, 'alice');
 INSERT INTO myTable (id, name) VALUES (null, 'john');
