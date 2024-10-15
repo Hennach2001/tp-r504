11ee9d4a6872613d2ad6bf4921ba570160d4ccf1
hennach <mohammed.hennach@univ-rouen.fr>
2024-10-15 03:51:56 -0700

    en cours

--- TP9/ServeurUDP.java
+++ TP9/ServeurUDP.java
@@ -1,22 +1,16 @@
-import java . io . * ;
-import java . net . * ;
+import java.io.*;
+import java.net.*;
 
-public class ServeurUDP
-{
-	public static void main( String[] args )
-	{
-		try
-		{
-			DatagramSocket stock = new DatagramSocket(1234);
-			while(true)
-			{
-			Systeme.out.println("-Waiting data");
-			DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
-			sock.recive(packet);
-			String str = new String(packet.getData() );
-			System.out.println("str="+str);
+public class ServeurUDP { 
+    public static void main(String[] args) throws Exception { 
+        DatagramSocket sock = new DatagramSocket(1234);
+        while (true) {
+            System.out.println("-Waiting data"); 
+            DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+            sock.receive(packet); 
+            String str = new String(packet.getData(), 0, packet.getLength()); 
+            System.out.println("str=" + str);
+        } 
+    } 
 }
-	}
-		}
-			}
 
